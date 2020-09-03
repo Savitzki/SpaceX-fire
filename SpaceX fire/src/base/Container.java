fac26a4425e9131eda6a0abbcbb2c649180bc759
Savitzki <marinasavitzki@gmail.com>
2020-09-03 12:41:15 -0300

    *inserção da imagens, metodos e atributos das classes container e fase

--- SpaceX fire/src/base/Container.java
+++ SpaceX fire/src/base/Container.java
@@ -1,5 +1,45 @@
 package base;
 
-public class Container {
+import javax.swing.JFrame;
 
+import modelo.Fase;
+
+public class Container extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Container() {
+		
+		//Adicionando um componente ao JFrame
+		add(new Fase());
+		
+		//tamanho do JFrame
+		setSize(1080, 720);
+		
+		//titulo do JFrame
+		setTitle("SpaceX fire");
+		
+		//Operação padrao para quando a janela for fechada
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//propriedade para colocar em tela cheia
+		this.setResizable(false);
+		
+		//Localização da tela, null é centralizada
+		setLocationRelativeTo(null);
+		
+		//visibilidade da tela
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		//instancia de novo objeto
+		new Container();
+	}
+
+	
 }
--- SpaceX fire/src/modelo/Fase.java
+++ SpaceX fire/src/modelo/Fase.java
@@ -1,5 +1,36 @@
 package modelo;
 
-public class Fase {
+
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Fase extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Image image;
+
+	public Fase() {
+	
+		//Criando uma icone para importar a imagem de fundo
+		ImageIcon ref = new ImageIcon("img\\background.png");
+		image = ref.getImage();
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+		Graphics2D grafic = (Graphics2D) g;
+		//desenhando a imagem no painel do jframe
+		grafic.drawImage(image, 0, 0, null);
+		
+	}
 
 }
